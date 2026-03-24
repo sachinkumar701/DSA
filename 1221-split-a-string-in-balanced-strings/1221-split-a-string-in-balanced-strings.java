fd50742e5cb3b53d32b641570914339044e8b1e4
SACHIN KUMAR <sachinkumartech83@gmail.com>
2026-03-24 16:28:32 +0530

    Time: 0 ms (100%), Space: 42.8 MB (34.97%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int count=0,balance=0;
+        for(char c:s.toCharArray()){
+            if(c=='L') balance++;
+            else balance--; 
+            if(balance==0) count++;
+        }
+        return count;
+    }
+}
