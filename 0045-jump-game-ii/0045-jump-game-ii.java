c1d60144ad18c744715e8f8fb2d4709261ff36e1
SACHIN KUMAR <sachinkumartech83@gmail.com>
2026-05-25 12:33:35 +0530

    Time: 1 ms (99.6%), Space: 47.2 MB (74.03%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,28 +1,15 @@
 class Solution {
     public int jump(int[] nums) {
-        int n = nums.length;
-        if (n <= 1) return 0;
-
-        int jumps = 0;
-        int currEnd = 0;
-        int maxReach = 0;
-
-        // Process indices until second last.
-        for (int i = 0; i < n - 1; i++) {
-            maxReach = Math.max(maxReach, i + nums[i]);
-
-            // End of current range => commit one jump.
-            if (i == currEnd) {
-                jumps++;
-                currEnd = maxReach;
-
-                // If this range already covers last index, stop.
-                if (currEnd >= n - 1) break;
+        int l = 0, r = 0, jumps = 0;
+        while (r < nums.length - 1) {
+            int maxi = 0;
+            for (int i = l; i <= r; i++) {
+                maxi = Math.max(maxi, i + nums[i]);
             }
+            l = r + 1;
+            r = maxi;
+            jumps++;
         }
-
         return jumps;
     }
 }
-
-  
