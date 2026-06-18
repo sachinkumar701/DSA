52fb32a73446950a558bc81fe0aa77b2a326cc4f
SACHIN KUMAR <sachinkumartech83@gmail.com>
2026-06-18 11:23:31 +0530

    Time: 1 ms (99.72%), Space: 47.5 MB (21.05%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,15 +1,25 @@
 class Solution {
+
     public int jump(int[] nums) {
-        int l = 0, r = 0, jumps = 0;
-        while (r < nums.length - 1) {
-            int maxi = 0;
-            for (int i = l; i <= r; i++) {
-                maxi = Math.max(maxi, i + nums[i]);
+
+        int jumps = 0;
+        int maxReach = 0;
+        int end = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            maxReach = Math.max(
+                    maxReach,
+                    i + nums[i]);
+
+            if (i == end) {
+
+                jumps++;
+
+                end = maxReach;
             }
-            l = r + 1;
-            r = maxi;
-            jumps++;
         }
+
         return jumps;
     }
 }
